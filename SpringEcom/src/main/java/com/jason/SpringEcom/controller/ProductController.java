54134e87f1819e9bfa86d44f125693382d162752
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2024-09-10 19:07:52 +0530

    Create Controller, Repo and Service

--- SpringEcom/src/main/java/com/jason/SpringEcom/controller/ProductController.java
+++ SpringEcom/src/main/java/com/jason/SpringEcom/controller/ProductController.java
@@ -1,15 +1,24 @@
 package com.jason.SpringEcom.controller;
 
+import com.jason.SpringEcom.model.Product;
+import com.jason.SpringEcom.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class ProductController {
 
+    @Autowired
+    private ProductService productService;
+
     @GetMapping("/products")
-    public String getProducts(){
-        return "All Products";
+    public List<Product> getProducts(){
+        return productService.getAllProducts();
     }
 }
---
+++ SpringEcom/src/main/java/com/jason/SpringEcom/service/ProductService.java
@@ -0,0 +1,19 @@
+package com.jason.SpringEcom.service;
+
+import com.jason.SpringEcom.model.Product;
+import com.jason.SpringEcom.repo.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepo productRepo;
+
+    public List<Product> getAllProducts() {
+        return productRepo.findAll();
+    }
+}
