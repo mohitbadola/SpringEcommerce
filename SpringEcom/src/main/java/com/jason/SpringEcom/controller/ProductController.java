bd0812f558582830f4abe9db67012a7e83dc68ea
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2024-09-12 03:50:05 +0530

    Create UpdateProduct

--- SpringEcom/src/main/java/com/jason/SpringEcom/controller/ProductController.java
+++ SpringEcom/src/main/java/com/jason/SpringEcom/controller/ProductController.java
@@ -58,8 +58,17 @@ public class ProductController {
         } catch (IOException e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
-
     }
 
+    @PutMapping("/product/{id}")
+    public ResponseEntity<String>UpdateProduct(@PathVariable int id, @RequestPart Product product, @RequestPart MultipartFile imageFile){
+        Product updatedProduct = null;
+        try {
+            updatedProduct = productService.updateProduct(product, imageFile);
+            return new ResponseEntity<>("Updated", HttpStatus.OK);
+        } catch (IOException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
 
 }
